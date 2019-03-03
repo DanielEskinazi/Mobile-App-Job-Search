9d20df83cb86df9616187e19caeddc9891f55311
Daniel Eskinazi <deski001@fiu.edu>
2019-03-03 14:15:44 -0500

    allow push notifications

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet} from 'react-native';
+import Expo, { Notifications } from 'expo';
+import { StyleSheet, Alert} from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/es/integration/react';
 import configureStore from './store'
@@ -7,7 +8,25 @@ import configureStore from './store'
 import AppNavigation from './navigation/AppNavigation';
 //import store from './store';
 
+import registerForNotifications from './services/push_notifications';
+
+
 class App extends React.Component {
+
+  componentDidMount() {
+    registerForNotifications();
+    Notifications.addListener((notification) => {
+      const { data: { text }, origin } = notification;
+      if (origin === 'received' && text){
+        Alert.alert(
+          'New Push Notification',
+          text,
+          [{ text: 'Ok.' }]
+        );
+      }
+    })
+  }
+
   render() {
     const { persistor, store } = configureStore();
     return(
